304faa0f2541c96955fd68dfccde76994bf73d9d
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-05-09 22:04:13 +0900

    20220509 ファイル分割

---
+++ router.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
+	"github.com/lafleurblanche/echoapisample/handler"
+)
+
+func newRouter() *echo.Echo {
+    e := echo.New()
+
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
+    
+    api := e.Group("/api")
+    
+    api.GET("/hello", handler.GetHello) // GET /api/hello
+
+    return e
+}
