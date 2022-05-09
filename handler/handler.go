c655d155b1498004f5c22a7c296776f7290bd88e
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-05-09 22:04:31 +0900

    20220509 ファイル分割

---
+++ handler/handler.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+    "net/http"
+    
+    "github.com/labstack/echo"
+)
+
+func GetHello(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
+}
